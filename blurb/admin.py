496c370fa3ae615a2653984247ffdaa76e5987c1
Namgyu Ho <itsnamgyu@gmail.com>
2019-07-12 10:06:57 -0700

    update blurb admin page
    Better list display and filtering

--- blurb/admin.py
+++ blurb/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from .models import Blurb
 
-admin.site.register(Blurb)
+#admin.site.register(Blurb)
--- blurb/models.py
+++ blurb/models.py
@@ -1,6 +1,7 @@
 import textwrap
 
 from django.db import models
+from django.contrib import admin
 
 
 class Blurb(models.Model):
@@ -25,3 +26,45 @@ class Blurb(models.Model):
             content = '<EMPTY>'
         content = textwrap.shorten(content, width=80, placeholder='...')
         return '[{}] {}'.format(self.identifier, content)
+
+
+class BlurbFilledFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = 'Fill Status'
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'filled'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        return (('true', 'Filled'), ('false', 'Not Filled'))
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        if self.value() == 'true':
+            return queryset.filter(content__isnull=False)
+        if self.value() == 'false':
+            return queryset.filter(content__isnull=True)
+
+
+@admin.register(Blurb)
+class BlurbAdmin(admin.ModelAdmin):
+    list_display = ('identifier', 'content', 'filled')
+    list_filter = (BlurbFilledFilter, )
+    ordering = ('identifier', )
+
+    def filled(self, blurb):
+        return blurb.content is not None
+
+    filled.boolean = True
