5d4b9294a61ebe12de52352b33f6312ecfecef36
Namgyu Ho <itsnamgyu@gmail.com>
2019-07-11 19:58:59 -0700

    update blurb system

--- blurb/templatetags/blurb.py
+++ blurb/templatetags/blurb.py
@@ -1,5 +1,7 @@
 from django import template
 from django.conf import settings
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 from blurb.exceptions import BlurbNotFilledException
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
@@ -8,20 +10,30 @@ from ..models import Blurb
 
 register = template.Library()
 
+EMPTY_BLURB = '[Empty Blurb "{}"] <a href="{}">Enter initial content as admin <i class="fas fa-edit"></i></a>'
+EDIT_LINK = ' <a href="{}"><i class="fas fa-edit"></i></a>'
 
-@register.simple_tag()
-def blurb(identifier):
+
+@register.simple_tag(takes_context=True)
+def blurb(context, identifier):
     # identifier should be in the form of '<page>:<name>'
     try:
         blurb = Blurb.objects.all().get(identifier=identifier)
     except ObjectDoesNotExist:
         blurb = Blurb.objects.create(identifier=identifier, content=None)
 
-    empty_message = '[DEBUG] Fill in the blurb "{}" in the admin console'
+    user = context.request.user
+    is_superuser = user and user.is_superuser
+    admin_link = reverse('admin:blurb_blurb_change', args=(blurb.id, ))
+
     if blurb.content == None:
         if settings.DEBUG:
-            return empty_message.format(identifier)
+            empty_message = EMPTY_BLURB.format(identifier, admin_link)
+            return mark_safe(empty_message)
         else:
             raise BlurbNotFilledException(identifier)
     else:
-        return blurb.content
+        content = blurb.content
+        if is_superuser:
+            content = blurb.content + EDIT_LINK.format(admin_link)
+        return mark_safe(content)
--- example/urls.py
+++ example/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('',
          TemplateView.as_view(template_name='example/index.html'),
          name='index'),
+    path('blurb_test',
+         TemplateView.as_view(template_name='example/blurb_test.html'),
+         name='blurb_test'),
 ]
