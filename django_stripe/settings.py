5cef01eee014805d09b711597f1927adc1d2fa35
Namgyu Ho <itsnamgyu@gmail.com>
2019-07-21 18:40:25 -0700

    fix syntax error

--- django_stripe/settings.py
+++ django_stripe/settings.py
@@ -1,10 +1,8 @@
 from django.conf import settings
 
-from app.settings import STRIPE_WEBHOOK_SIGNING_SECRET
+PUBLIC_KEY: str = getattr(settings, 'STRIPE_PUBLIC_KEY', None)
+SECRET_KEY: str = getattr(settings, 'STRIPE_SECRET_KEY', None)
+WEBHOOK_SIGNING_SECRET: str = getattr(settings,
+                                      'STRIPE_WEBHOOK_SIGNING_SECRET', None)
 
-PUBLIC_KEY: str = getattr(settings, STRIPE_PUBLIC_KEY, None)
-SECRET_KEY: str = getattr(settings, STRIPE_SECRET_KEY, None)
-WEBHOOK_SIGNING_SECRET: str = getattr(settings, STRIPE_WEBHOOK_SIGNING_SECRET,
-                                      None)
-
-SUPPORT_EMAIL: str = getattr(settings, STRIPE_SUPPORT_EMAIL, None)
+SUPPORT_EMAIL: str = getattr(settings, 'STRIPE_SUPPORT_EMAIL', None)
