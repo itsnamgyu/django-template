f9e5d01cccb214c8cfeb5d78541e5ac0e2158ce9
Namgyu Ho <itsnamgyu@gmail.com>
2019-07-28 00:09:23 +0000

    add static_root settings

--- app/settings.py
+++ app/settings.py
@@ -125,6 +125,8 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+if not DEBUG:
+    STATIC_ROOT = require_env('STATIC_ROOT')
 
 LOGIN_REDIRECT_URL = '/'
 ACCOUNT_LOGOUT_REDIRECT_URL = '/'
