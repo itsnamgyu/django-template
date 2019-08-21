486147b2cc6e18f59ca2e04e98916a2c9c7b8604
Namgyu Ho <itsnamgyu@gmail.com>
2019-08-21 15:41:53 -0700

    Remove old preview URL

--- base/urls.py
+++ base/urls.py
@@ -4,8 +4,4 @@ from django.views.generic import TemplateView
 from . import views
 
 app_name = 'base'
-urlpatterns = [
-    path('preview',
-         TemplateView.as_view(template_name='base/preview.html'),
-         name='preview'),
-]
+urlpatterns = []
