ee987766e43502e06312754e0c0adf5b058928a1
Namgyu Ho <itsnamgyu@gmail.com>
2020-01-03 17:38:47 -0800

    Move allauth templates to base
    Requires base to be above allauth in INSTALLED APPS

--- app/settings.py
+++ app/settings.py
@@ -56,11 +56,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.sites",
+    "base",
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
     "bootstrap4",
-    "base",
     # Carousel
     "versatileimagefield",
     "carousel",
