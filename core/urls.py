1c41c7ef3901d03b74c46248dd2b4280dd5690c7
Namgyu Ho <itsnamgyu@gmail.com>
2020-01-03 22:49:39 -0800

    Apply dt_content system to core app

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,11 @@ from django.views.generic import TemplateView
 
 from .views import *
 
+app_name = "core"
+
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
-
+    path("page/<path:menu_path>/", PageView.as_view(), name="page"),
+    path("page/", PageView.as_view(), name="page-base"),
 ]
+
